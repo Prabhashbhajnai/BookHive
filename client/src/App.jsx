4ad3e855ae0938f1f0b96696c4e3db0502fa9267
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-11-09 12:05:30 +0530

    solved the redirect issue from home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Redirect} from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 //HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 import BooksLayoutHOC from "./HOC/Books.HOC";
@@ -22,30 +22,30 @@ import Romance from "./Page/BooksType/Romance";
 
 function App() {
   return (
-  <>
-   {/* <HomeLayoutHOC path="/" exact component={Temp} />*/}
-    {/*<HomeLayoutHOC path="/" exact component={Homepage} />*/}
-    <Route path="/"exact>
-      <Redirect to="/:type" />
-    </Route>
-    <Route path="/books/:id" exact>
-      <Redirect to="/books/:id/humor" />
-    </Route>
-    <HomeLayoutHOC path="/:type" exact component={Homepage} />
- 
-    <BooksLayoutHOC path="/books/:id/humor" exact component={Humor} /> 
-    <BooksLayoutHOC path="/books/:id/fiction" exact component={Fiction} />
-    <BooksLayoutHOC path="/books/:id/science-fic" exact component={ScFi} />
-    <BooksLayoutHOC path="/books/:id/romance" exact component={Romance} />
-    <BooksLayoutHOC path="/books/:id/science-tech" exact component={ScTech} />
-    <BooksLayoutHOC path="/books/:id/mystry" exact component={Mystry} />
-    <BooksLayoutHOC path="/books/:id/bio-auto-graphy" exact component={BioAuto} />
-    <BooksLayoutHOC path="/books/:id/fable" exact component={Fable} />
-	<BooksLayoutHOC path="/books/:id/horror" exact component={Horror} />
-	<BooksLayoutHOC path="/books/:id/s-story" exact component={ShortStory} />
-	<BooksLayoutHOC path="/books/:id/poem" exact component={Poetry} />
+    <>
+      {/* <HomeLayoutHOC path="/" exact component={Temp} />*/}
+      {/*<HomeLayoutHOC path="/" exact component={Homepage} />*/}
+      <Route path="/" exact>
+        <Redirect to="/:type" />
+      </Route>
+      <Route path="/books/:id" exact>
+        <Redirect to="/books/:id/humor" />
+      </Route>
+      <HomeLayoutHOC path="/:type" exact component={Homepage} />
 
-  </>
+      <BooksLayoutHOC path="/books/:id/humor" exact component={Humor} />
+      <BooksLayoutHOC path="/books/:id/fiction" exact component={Fiction} />
+      <BooksLayoutHOC path="/books/:id/science-fic" exact component={ScFi} />
+      <BooksLayoutHOC path="/books/:id/romance" exact component={Romance} />
+      <BooksLayoutHOC path="/books/:id/science-tech" exact component={ScTech} />
+      <BooksLayoutHOC path="/books/:id/mystry" exact component={Mystry} />
+      <BooksLayoutHOC path="/books/:id/bio-auto-graphy" exact component={BioAuto} />
+      <BooksLayoutHOC path="/books/:id/fable" exact component={Fable} />
+      <BooksLayoutHOC path="/books/:id/horror" exact component={Horror} />
+      <BooksLayoutHOC path="/books/:id/s-story" exact component={ShortStory} />
+      <BooksLayoutHOC path="/books/:id/poem" exact component={Poetry} />
+
+    </>
   );
 }
 
--- client/src/Components/SourcesCard.jsx
+++ client/src/Components/SourcesCard.jsx
@@ -1,48 +1,49 @@
 import React from "react";
-import {AiTwotoneStar} from "react-icons/ai";
+import { AiTwotoneStar } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const SourcesCard = (props) => {
     return (
-    <>
-    <div className="bg-white p-4 w-full mb-4 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg  md:w-1/2 lg:w-1/4">
-         <div className="w-full h-56 lg:64 relative">
-             <div className="absolute w-full bottom-4 flex items-end justify-between">
-               <div className="flex flex-col gap-2 items-start ">
-                   {props.isPro && (
-                       <span className="bg-red-400 text-white px-2 py-1 rounded text-sm">
-                           Pro extra 10% off
-                           </span>
-                    )}
-                    {props.isOff && (
-                            <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
-                                ₹{props.isOff}OFF 
-                            </span>
-                    )}
-               </div>
-               <span className="bg-white bg-opacity-75 p-1 rounded mr-3">
-                   {props.duration} min
-                </span>
-             </div>
-             <img
-               src={props.photos.length && props.photos[0] }               
-               alt="Books"
-               className="w-full h-full rounded-2xl"
-               />
-         </div>
-         <div className=" my-2 flex flex-col gap-2">
-             <div className="flex items-center justify-between">
-                 <h4 className="text-xl font-medium">{props.name}</h4>
-                 <span className="bg-green-800 text-white text-sm flex items-center p-1 rounded  ">
-                    {props.booksReviewValue} <AiTwotoneStar />
-                 </span>
-             </div>
-             <div className="flex items-center justify-between text-gray-500">
-                 <p>{props.category.join(", ")}</p>
-                 <p>₹ {props.averageCost} for one</p>
-             </div>
-         </div>
-    </div>
-    </>
+        <Link to={`/${props.name}/${props._id}`} className="w-full md:w-1/3 lg:w-1/4">
+            <div className="bg-white p-2 w-full mb-4 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg">
+                <div className="w-full h-56 lg:64 relative">
+                    <div className="absolute w-full bottom-4 flex items-end justify-between">
+                        <div className="flex flex-col gap-2 items-start ">
+                            {props.isPro && (
+                                <span className="bg-red-400 text-white px-2 py-1 rounded text-sm">
+                                    Pro extra 10% off
+                                </span>
+                            )}
+                            {props.isOff && (
+                                <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
+                                    ₹{props.isOff}OFF
+                                </span>
+                            )}
+                        </div>
+                        <span className="bg-white bg-opacity-75 p-1 rounded mr-3">
+                            {props.duration} min
+                        </span>
+                    </div>
+                    <img
+                        src={props.photos.length && props.photos[0]}
+                        alt="Books"
+                        className="w-full h-full rounded-2xl"
+                    />
+                </div>
+                <div className=" my-2 flex flex-col gap-2">
+                    <div className="flex items-center justify-between">
+                        <h4 className="text-xl font-medium">{props.name}</h4>
+                        <span className="bg-green-800 text-white text-sm flex items-center p-1 rounded  ">
+                            {props.booksReviewValue} <AiTwotoneStar />
+                        </span>
+                    </div>
+                    <div className="flex items-center justify-between text-gray-500">
+                        <p>{props.category.join(", ")}</p>
+                        <p>₹ {props.averageCost} for one</p>
+                    </div>
+                </div>
+            </div>
+        </Link>
     );
 };
 
